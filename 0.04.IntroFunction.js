dd448428bb1f169128005f3e41ff9a56451d9a94
ilhamhafidz404 <xxspanzie@gmail.com>
2022-08-23 21:38:46 +0700

    Latihan Function pada Javascript(Intro)

---
+++ 0.04.IntroFunction.js
@@ -0,0 +1,15 @@
+// Build-in function adalah function yang sudah disediakan oleh javascript
+// user-define function adalah functionn yang kita buat sendiri
+
+// function declaration
+function jumlahkanNilai(a, b) {
+  // a dan b disebut parameter
+  let total = a + b;
+  return total;
+}
+
+// function expression
+const kurangiNilai = function () {
+  let total = a - b;
+  return total;
+};
